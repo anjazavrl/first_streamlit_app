ffd9b667d5c5e6683cb80e851a425d75d661a8ea
anjazavrl <zavrl.anja@gmail.com>
2023-07-26 08:18:54 +0200

    Update streamlit_app.py
    added new line and emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Heathy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 3 & Bluebarry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Bluebarry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
